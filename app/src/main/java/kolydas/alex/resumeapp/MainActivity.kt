5d71846a28a8dedfcb87187085e9ea0c2b144ec4
ArekkusuTaichou <alexkolydas@gmail.com>
2018-08-27 20:26:01 +0300

    adding a button to main activity

--- app/src/main/java/kolydas/alex/resumeapp/MainActivity.kt
+++ app/src/main/java/kolydas/alex/resumeapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package kolydas.alex.resumeapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var workHistory =findViewById<Button>(R.id.workBtn)
+
+        workHistory.setOnClickListener{
+            //Moves to next activity
+            var workHistory=Intent(getApplicationContext(),WorkExperience::class.java)
+            startActivity(workHistory)
+
+
+        }
     }
 }
