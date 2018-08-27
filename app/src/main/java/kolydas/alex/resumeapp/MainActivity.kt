ef64f15e7a3645d52e4bd42895ceb0a12be26789
ArekkusuTaichou <alexkolydas@gmail.com>
2018-08-27 20:59:57 +0300

    Last Commit

--- app/src/main/java/kolydas/alex/resumeapp/MainActivity.kt
+++ app/src/main/java/kolydas/alex/resumeapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package kolydas.alex.resumeapp
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,11 +13,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var workHistory =findViewById<Button>(R.id.workBtn)
+        var callButton=findViewById<Button>(R.id.callBtn)
+        var emailButton=findViewById<Button>(R.id.emailBtn)
 
         workHistory.setOnClickListener{
             //Moves to next activity
-            var workHistory=Intent(getApplicationContext(),WorkExperience::class.java)
-            startActivity(workHistory)
+            var workHistoryIntent=Intent(getApplicationContext(),WorkExperience::class.java)
+            startActivity(workHistoryIntent)
+        }
+
+        callButton.setOnClickListener{
+            //call the number
+            var phoneUri= Uri.parse("tel:<ENTER YOUR PHONE>")
+            var callIntent=Intent(Intent.ACTION_DIAL,phoneUri)
+            startActivity(callIntent)
+        }
+
+        emailButton.setOnClickListener{
+            //send an email into this email
+            var emailIntent=Intent(Intent.ACTION_SEND)
+            emailIntent.setType("plain/text")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL,"<ENTER YOUR EMAIL>")
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT,"Nice Resume!")
+            emailIntent.putExtra(Intent.EXTRA_TEXT,"I Really enjoy your resume")
+            startActivity(emailIntent)
 
 
         }
